87bafed89a89b3883020f2513450220636034533
lerning_php <longt9967@gmail.com>
2025-12-09 13:08:36 +0700

    php

---
+++ hocphp/bai1.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>tai lieu hoc tap lap trinh web</h1>
+    <?php
+    echo "<hr>";
+    ?>
+    <p> tai lieu hoc html </p>
+    <p> tai lieu hoc css </p>
+    <?php
+    echo "<h2> tai lieu hoc javascript </h2>";
+    echo "<h3> tai lieu hoc mysql </h3>";
+    echo "<h4> tai lieu hoc php </h4>";
+    ?>
+    <hr>
+    <?php
+    $text = "tu co ban"." "."den nang cao";
+    echo $text;
+    ?>
+</body>
+</html>
